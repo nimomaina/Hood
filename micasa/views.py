fecff31c117f3b7c9b5e23b468b166f912b6e2cf
nimomaina <nimomaina.nm@gmail.com>
2019-03-24 15:34:07 +0300

    Create views

--- micasa/views.py
+++ micasa/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from .models import *
+from django.contrib import messages
+from . forms import *
+from django.contrib.auth.models import User
 # Create your views here.
+
+
+def search_category(request):
+    location = Location.objects.all()
+    category = Category.objects.all()
+    if 'Category' in request.GET and request.GET["Category"]:
+        category = request.GET.get("Category")
+        searched_image = Picture.search_by_category(category)
+        message = f"{category}"
+
+        return render(request,'category/searched.html', {"message":message,"Category":searched_image})
+
+    else:
+        message = "You haven't searched for anything"
+        return render(request,'category/searched.html',{"message":message})
+
+
+def filter_location(request):
+    locations = Location.objects.all()
+    location = request.GET.get("location")
+
+    searched_image = Picture.filter_by_location(location)
+    message = f"{location}"
+
+    return render(request,'category/location.html', {"message":message,"location":searched_image, "locations":locations})
+
+
+
